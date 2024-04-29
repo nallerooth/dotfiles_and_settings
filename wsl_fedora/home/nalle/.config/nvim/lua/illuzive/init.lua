728329f5bd48fa6733bbf771d5018c8a6a860c72
Nalle Rooth <nalle.rooth@gmail.com>
2024-04-29 19:02:08 +0200

    Added setup for wsl_fedora

---
+++ wsl_fedora/home/nalle/.config/nvim/lua/illuzive/init.lua
@@ -0,0 +1,4 @@
+require("illuzive.set")
+require("illuzive.strip_whitespace")
+require("illuzive.packer")
+require("illuzive.remap")
---
+++ wsl_fedora/home/nalle/.config/nvim/lua/illuzive/remap.lua
@@ -0,0 +1,29 @@
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>ec", "<cmd>tabnew $HOME/.config/nvim/<CR>")
+
+vim.keymap.set("n", "<C-u>", vim.cmd.UndotreeToggle)
+
+-- Move lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- Scrolling without moving cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Yanking
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Quickfix
+vim.keymap.set("n", "<leader>-h", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>-l", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
