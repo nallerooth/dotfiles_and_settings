8cb4987be58099d78bc5b99a19581265d1d91c47
Nalle Rooth <nalle.rooth@gmail.com>
2025-09-21 10:59:30 +0200

    Re-added some previous keybinds

--- munin/home/nalle/.config/nvim/lua/illuzive/base.lua
+++ munin/home/nalle/.config/nvim/lua/illuzive/base.lua
@@ -1,10 +1,27 @@
--- Map leader key so that everything else uses this mapping
+-- Map leader key first, so that all plugins get the correct leader
 vim.g.mapleader = " "
 
 -- Misc keymaps
 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
 vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ec", "<cmd>tabnew $HOME/.config/nvim/<CR>")
+
+-- Keep cursor in the middle of the screen when navigating
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Yanking
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Quickfix
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Editor preferences
 vim.opt.shiftwidth = 4
