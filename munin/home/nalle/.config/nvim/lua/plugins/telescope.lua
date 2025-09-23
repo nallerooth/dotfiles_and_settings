4e1931bb071db6d345cff7e93111e38f68e07919
Nalle Rooth <nalle.rooth@paligo.net>
2025-09-23 15:21:53 +0200

    Fixed malformed telescope config

--- munin/home/nalle/.config/nvim/lua/plugins/telescope.lua
+++ munin/home/nalle/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,17 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffers" })
-
 return {
     'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		require('telescope').setup({
+			defaults = {
+				file_ignore_patterns = { "node_modules", ".git/" },
+			},
+		})
+
+		local builtin = require('telescope.builtin')
+
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffers" })
+	end
 }
