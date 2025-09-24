659bf37c008ae59e434b5d78626ff1c7950feebb
Nalle Rooth <nalle.rooth@paligo.net>
2025-09-24 14:32:20 +0200

    Added automated organization of goimports
    - Also updated lazy-lock.json file

--- munin/home/nalle/.config/nvim/lua/illuzive/autocommand.lua
+++ munin/home/nalle/.config/nvim/lua/illuzive/autocommand.lua
@@ -1,1 +1,2 @@
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.cmd [[autocmd BufWritePre *.go :silent! lua vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })]]
